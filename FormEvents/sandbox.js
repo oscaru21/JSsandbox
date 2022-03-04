56eb1808dd14a30fedea0e57f88c8dc44cb5661c
Oscar Umaña <oscarumana1021@gmail.com>
2022-03-03 18:13:33 -0600

    keyup update

--- FormEvents/sandbox.js
+++ FormEvents/sandbox.js
@@ -1,15 +1,25 @@
 const form = document.querySelector('form');
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    const pattern = /^[a-z]{6,}$/;
-    if(!pattern.test(form.username.value)){
-        form.username.style.borderColor = 'red';
-    }else{
-        form.username.style.borderColor = 'black';
-    }
-    console.log(form.username.value);
-    console.log(form.password.value);
+   //validation
+   const username = form.username.value;
+   
+   if(usernamePattern.test(username)){
+       feedback.textContent = "that username is valid";
+   }else{
+       feedback.textContent = "username is not valid!";
+   }
 })
 
-//testing regular expressions
+//live feedback
+
+form.username.addEventListener('keyup', e => {
+    if(usernamePattern.test(e.target.value)){
+        e.target.setAttribute('class', 'success');
+    }else{
+        e.target.setAttribute('class', 'error');
+    }
+})
