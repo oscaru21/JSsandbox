1b2afad0eb79cde87a704069a7141d0dcfdea040
Oscar Umaña <oscarumana1021@gmail.com>
2022-03-04 22:25:47 -0600

    completed todo list

---
+++ ToDoList/app.js
@@ -0,0 +1,43 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input')
+
+const generateTemplate = todo => {
+    return `
+    <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+        <span>${todo}</span>
+        <span class="delete">X</span>
+    </li>
+    `;
+};
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = e.target.add.value.trim();  
+    if(todo.length != 0){
+        list.innerHTML += generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+//delete elements
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filteredElements = (term) => {
+    Array.from(list.children)
+        .filter(li => !li.textContent.toLowerCase().includes(term.toLowerCase()))
+        .forEach(a => a.classList.add('filtered'));
+    Array.from(list.children)
+        .filter(li => li.textContent.toLowerCase().includes(term.toLowerCase()))
+        .forEach(a => a.classList.remove('filtered'));
+};
+
+//keyup listener
+search.addEventListener('keyup', e => {
+    let term = e.target.value.trim();
+    filteredElements(term);
+});
